2c23da1859238e6f689351218d3323411d6b1405
Violeta <violetalgil@gmail.com>
2022-09-28 13:59:21 +0200

    Agregar archivos de proyecto.

---
+++ BackEnd/Migrations/20220928070449_v1.0.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEnd.Migrations
+{
+    public partial class v10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aeropuertos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Ciudad = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(50)", nullable: false),
+                    M2 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aeropuertos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aeropuertos");
+        }
+    }
+}
---
+++ BackEnd/Model/Aeropuerto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEnd.Model
+{
+    [Table("Aeropuertos")]
+    public class Aeropuerto
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Nombre { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Ciudad { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Email { get; set; }
+        [Required]
+        public int M2 { get; set; }
+    }
+}
